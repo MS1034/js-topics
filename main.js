6027160ebf9638269c3f432b4353160dbbf3aa09
Muhammad Subhan <muhammadsubhan5701@gmail.com>
2024-07-10 14:34:16 +0500

    Refactor(Filter Table): Change Tailwind CSS to Bootstrap

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ let jsTopics = [
         link: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Manipulating_documents"
     }
 ]
+let svg = '<svg viewBox="0 0 1024 1024" fill="#000000" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M32 241.6c-11.2 0-20-8.8-20-20s8.8-20 20-20l940 1.6c11.2 0 20 8.8 20 20s-8.8 20-20 20L32 241.6zM186.4 282.4c0-11.2 8.8-20 20-20s20 8.8 20 20v688.8l585.6-6.4V289.6c0-11.2 8.8-20 20-20s20 8.8 20 20v716.8l-666.4 7.2V282.4z" fill=""></path><path d="M682.4 867.2c-11.2 0-20-8.8-20-20V372c0-11.2 8.8-20 20-20s20 8.8 20 20v475.2c0.8 11.2-8.8 20-20 20zM367.2 867.2c-11.2 0-20-8.8-20-20V372c0-11.2 8.8-20 20-20s20 8.8 20 20v475.2c0.8 11.2-8.8 20-20 20zM524.8 867.2c-11.2 0-20-8.8-20-20V372c0-11.2 8.8-20 20-20s20 8.8 20 20v475.2c0.8 11.2-8.8 20-20 20zM655.2 213.6v-48.8c0-17.6-14.4-32-32-32H418.4c-18.4 0-32 14.4-32 32.8V208h-40v-42.4c0-40 32.8-72.8 72.8-72.8H624c40 0 72.8 32.8 72.8 72.8v48.8h-41.6z" fill=""></path></g></svg>'
 
 
 // Generate unique id for each topic
@@ -65,6 +66,15 @@ function renderTableRows(
         html+= `<tr id= "${topic.id}" style="display: ${topic.isChecked? "table-row" : " none"} " onclick="toggleSelectionRow()"> <td> <input  type="checkbox" class= "cb-visibility ml-4" >   <label for="select-all-cb" class="ml-1">${topic.title}</label> </td> 
         <td> ${durationStr(topic.time)}</td>
         <td>  <a href="${topic.link}" class ="ubuntu">${topic.link} </a> </td>
+       <td> <button type="button"
+                    class="btn btn-lg btn-outline-primary">
+                <i class="fa fa-edit"></i>
+            </button> </td> </td>
+       <td> <button type="button"
+                    class="btn btn-lg btn-outline-danger">
+                <i class="fa fa-trash"></i>
+            </button> </td>
+
         </tr>`
     }
     )
@@ -90,7 +100,6 @@ function toggleSelectionMaster(cb)
 
 function toggleSelectionRow()
 {
-    let table = document.getElementById("js-table")
     let flag = true;
     for(let i = 0; i<table.rows.length; i++)
     {
